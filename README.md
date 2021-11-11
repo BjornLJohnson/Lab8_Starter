e30d0d2a88ff7085d69086515602a80e60de5324
Bjorn <bjorn@lundjohnson.com>
2021-11-11 13:47:31 -0800

    did it

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 8 - Starter
+
+Bjorn Johnson
+
+1. Within a Github action that runs whenever code is pushed 
+2. No
+3. No, for a messagine application, a "message" feature allowing a user to write and send a messag to another user would require a number of different components to interact. This would include multiple functions to all be performing properly, so an end to end test would be more appropriate. This is a test that involves emulating user actions from start to finish, so it would be broader than a single unit test.
+4. Yes, this functionality is probably implemented with a single function, and is thus a good candidate for unit testing. It does not require emulating the entire user experience.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,72 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// isPhoneNumber
+test('valid phone number 1', () => {
+    expect(functions.isPhoneNumber("(123)-456-7890")).toBe(true);
+});
+test('valid phone number 2', () => {
+    expect(functions.isPhoneNumber("(408)-111-1111")).toBe(true);
+});
+test('invalid phone number 1', () => {
+    expect(functions.isPhoneNumber("1223AB-5678")).toBe(false);
+});
+test('invalid phone number 2', () => {
+    expect(functions.isPhoneNumber("0")).toBe(false);
+});
+
+// isEmail
+test('valid email 1', () => {
+    expect(functions.isEmail("bjorn@lundjohnson.com")).toBe(true);
+});
+test('valid email 2', () => {
+    expect(functions.isEmail("fakemail@gmail.com")).toBe(true);
+});
+test('invalid email 1', () => {
+    expect(functions.isEmail("test@fake.mail.net")).toBe(false);
+});
+test('invalid email 2', () => {
+    expect(functions.isEmail("invalid@emailaddress")).toBe(false);
+});
+
+// isStrongPassword
+test('valid strong password 1', () => {
+    expect(functions.isStrongPassword("test")).toBe(true);
+});
+test('valid strong password 2', () => {
+    expect(functions.isStrongPassword("qwerty")).toBe(true);
+});
+test('invalid strong password 1', () => {
+    expect(functions.isStrongPassword("$a_g!pi#uf$")).toBe(false);
+});
+test('invalid strong password 2', () => {
+    expect(functions.isStrongPassword("4h5#$t2#$%YG45")).toBe(false);
+});
+
+// isDate
+test('valid date 1', () => {
+    expect(functions.isDate("1/2/1900")).toBe(true);
+});
+test('valid date 2', () => {
+    expect(functions.isDate("05/12/2021")).toBe(true);
+});
+test('invalid date 1', () => {
+    expect(functions.isDate("123/12/1999")).toBe(false);
+});
+test('invalid date 2', () => {
+    expect(functions.isDate("ABC")).toBe(false);
+});
+
+// isHexColor
+test('valid hex color 1', () => {
+    expect(functions.isHexColor("ABC")).toBe(true);
+});
+test('valid hex color 2', () => {
+    expect(functions.isHexColor("000000")).toBe(true);
+});
+test('invalid hex color 1', () => {
+    expect(functions.isHexColor("G13")).toBe(false);
+});
+test('invalid hex color 2', () => {
+    expect(functions.isHexColor("12345")).toBe(false);
+});
